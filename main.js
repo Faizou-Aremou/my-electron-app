9b5d6105c23a71ddc9469534b24cd2e4dc0f0530
Faïzou Aremou <faizouaremou@gmail.com>
2023-08-17 19:32:37 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+  const windows = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  windows.loadFile('index.html');
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});

7ae90b5eff6e64a636e089c47b2f4447866c35ec
Faizou Aremou <faizouaremou@gmail.com>
2023-08-19 14:29:19 +0200

    finish first electron application

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const { app, BrowserWindow } = require('electron');
-
+const path = require('path');
 const createWindow = () => {
   const windows = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
   });
 
   windows.loadFile('index.html');
@@ -11,6 +14,11 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow();
+  app.on('activate', () => {
+    if ((BrowserWindow.getAllWindows().length = 0)) {
+      createWindow();
+    }
+  });
 });
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {

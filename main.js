39494f0b9d702b3eb5d3bbd634323592db7fae9a
Faizou Aremou <faizouaremou@gmail.com>
2023-08-19 13:53:38 +0200

    update main js

--- main.js
+++ main.js
@@ -12,3 +12,8 @@ const createWindow = () => {
 app.whenReady().then(() => {
   createWindow();
 });
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
